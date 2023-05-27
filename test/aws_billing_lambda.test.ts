b22f98451dfd874652a2f43d6ba2a8aa766ef23d
nash1111 <ken1251213@gmail.com>
2023-05-27 23:50:40 +0900

    Initial commit

---
+++ test/aws_billing_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsBillingLambda from '../lib/aws_billing_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_billing_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsBillingLambda.AwsBillingLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
